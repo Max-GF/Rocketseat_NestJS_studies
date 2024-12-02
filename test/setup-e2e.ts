d3527abef7d4e628eb43fd2708064521287d0983
Max-GF <maxdultra@gmail.com>
2024-12-02 15:56:24 -0300

    test setup fix and factories refact to e2e testes

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,27 +1,32 @@
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
-import 'dotenv/config'
+import { config } from 'dotenv'
+import { envSchema } from '@/infra/env/env'
 
+config({ path: '.env', override: true })
+config({ path: '.env.test', override: true })
 let prisma: PrismaClient
+const env = envSchema.parse(process.env)
 const schemaId = randomUUID()
 
 function generateUniqueDataBaseURL(schemaId: string): string {
-    if (!process.env.DATABASE_URL) {
+    if (!env.DATABASE_URL) {
         throw new Error('Please provide a DATABASE_URL environment variable')
     }
-    const url = new URL(process.env.DATABASE_URL)
+    const url = new URL(env.DATABASE_URL)
     url.searchParams.set('schema', schemaId)
     return url.toString()
 }
 beforeAll(async () => {
     const databaseURL = generateUniqueDataBaseURL(schemaId)
     process.env.DATABASE_URL = databaseURL
-    execSync('pnpm prisma migrate deploy') // ele só roda as migrations no banco, já o dev ele gera novas se tiver mudanças 
+    console.log(process.env.DATABASE_URL)
     prisma = new PrismaClient()
+    execSync('pnpm prisma migrate deploy') // ele só roda as migrations no banco, já o dev ele gera novas se tiver mudanças 
 })
 afterAll(async () => {
-    const currentSchema = await prisma.$queryRawUnsafe('SELECT current_schema()')
+    // const currentSchema = await prisma.$queryRawUnsafe('SELECT current_schema()')
     await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
     await prisma.$disconnect()
 })
